f34024286534c6c4b2e973f4065837c1d72e1e31
kang sung hwan <kumi1688@gmail.com>
2020-04-19 08:05:16 +0900

    [Iteration1] make frontend

--- app/backend/index.js
+++ app/backend/index.js
@@ -5,11 +5,7 @@ var path = require('path');
 var bodyParser = require('body-parser');
 var cors = require('cors');
 
-const {client} = require('./mqtt');   //mqtt client 모듈
 var lights = require('./routes/light'); //light 라우터
-
-var hueStatus = lights.status   //hue들의 상태 정보를 갖고있는 배열
-var hueProperties = lights.properties   //hue 속성 정보 객체
 
 //socket.io 부분
 // app.io = require("socket.io")();
@@ -24,47 +20,6 @@ var hueProperties = lights.properties   //hue 속성 정보 객체
 //     });
 //   }, 2000);
 // });
-
-//mqtt 부분
-// client.on("connect", () => {
-//   console.log("connected : " + client.connected);
-// })
-
-// client.on("error", (error) => {
-//   console.log("Can't connect : " + error);
-// })
-
-// client.on('message', (topic, message, packet) => {
-//   //console.log("message is " + JSON.parse(message))
-//   console.log("topic is " + topic)
-//   if(topic == 'philstatus'){
-//     message = JSON.parse(message);    
-//     for(var i=0; i<hueStatus.length; i++){
-//       if(hueStatus[i].number == message.number){  //message.number에 해당하는 hue의 상태 업데이트
-//         hueStatus[i].power = message.power;
-//         hueStatus[i].hue = message.hue;
-//         hueStatus[i].brightness = message.brightness;
-//         hueStatus[i].saturation = message.saturation;
-//         hueStatus[i].colorTemperature = message.colorTemperature;
-//       }
-//     }
-//   }else if(topic == 'philinit'){
-//     //console.log("속성")
-//     hueProperties = JSON.parse(message)
-//     //console.log(properties);
-
-//     hueStatus = [] //hue들의 상태 정보 초기화
-//     for(var i=0; i<hueProperties.device; i++){
-//       hueStatus.push({number:hueProperties.number[i]}) //hue 번호를 hueStatus 배열에 저장
-//       client.publish("philipshue/"+(i+1), "state")   //각 조명의 상태 요청
-//     }
-//   }
-// })
-
-// client.subscribe("philinit"); //조명 속성 정보
-// client.subscribe("philstatus");    //조명 상태
-
-// client.publish("philipshue/1", "initstatus")    //처음 로딩됐을 때 조명 속성 정보 요청, philinit 토픽으로 받음
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
--- app/backend/mqtt.js
+++ app/backend/mqtt.js
@@ -6,16 +6,18 @@ const options = {
 }
 // subscribe 할 topic 목록
 let subscribeList = [
-    'res/hue/config', // 속성 정보 요청 응답
-    'res/hue/status', // 현재 정보 요청 응답
+    'res/hue/property', // 속성 정보 요청 응답
+    'res/hue/status', // 현재 상태 정보 요청 응답
 ];
 // publish 할 topic 목록
 let publishList = [
-    'req/hue/config',
-    'req/hue/status'
+    'req/hue/property', // 속성 정보 요청 
+    'req/hue/status', // 현재 상태 정보 요청 
+    'req/hue/changeStatus/+', // 현재 상태 변경 요청
 ];
+
 // hue 속성
-let hueConfig = {};
+let hueProperty = {};
 // hue 상태
 let hueStatus = [];
 
@@ -33,26 +35,44 @@ subscribeList.forEach(function(topic){
     client.subscribe(topic);
 });
 
-client.publish('req/hue/config', '');
+// 처음 로딩시 속성과 초기상태 요청
+client.publish('req/hue/property', '');
 client.publish('req/hue/status', '');
 
 client.on('message', (topic, message)=>{
-    if(topic === 'res/hue/config'){
-        hueConfig = JSON.parse(message);
-        // console.log(hueConfig);
+    console.log('topic : ', topic);
+    if(topic === 'res/hue/property'){
+        hueProperty = JSON.parse(message);
     }else if (topic === 'res/hue/status'){
         hueStatus = JSON.parse(message);
-        // console.log(hueStatus);
     }
 })
 
-function getHueConfig(){
-    return hueConfig;
+function getHueProperty(){
+    return hueProperty;
 }
 function getHueStatus(){
     return hueStatus;
 }
 
+function requestHueData(pubTopic, pubMessage){
+    return new Promise((resolve, reject)=>{
+        client.publish(pubTopic, pubMessage, {}, () => {
+           client.on('message', (subTopic, subMessage)=>{
+               console.log('subTopic: ', subTopic)
+               // publish topic 과 subscribe topic이 같아야 한다
+               if(pubTopic.split('/').splice(1).join('/') === subTopic.split('/').splice(1).join('/')){
+                hueStatus = JSON.parse(subMessage);
+                resolve(hueStatus);     
+               } 
+           }) 
+        });
+    })
+}
+
 module.exports = {
-    client, getHueConfig, getHueStatus
+    client, 
+    getHueProperty, // 속성 정보 요청
+    getHueStatus,  // 현재 상태 정보 요청 (정보가 최신이 아닐 수 있음)
+    requestHueData // 현재 상태 정보 요청 (최신 정보 받아옴)
 }
