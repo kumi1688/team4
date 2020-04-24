7518f866d5837075877789fdd7e9c5881eba090f
kumi1688 <kumi1688@ajou.ac.kr>
2020-04-24 09:14:43 +0900

    [Iteration2] commit before chnage layout

---
+++ app/frontend/src/components/common/drawer.vue
@@ -0,0 +1,72 @@
+<template>
+  <v-navigation-drawer v-model="drawer" :clipped="$vuetify.breakpoint.lgAndUp" app>
+    <v-list dense>
+      <template v-for="item in items">
+        <v-row v-if="item.heading" :key="item.heading" align="center">
+          <v-col cols="6">
+            <v-subheader v-if="item.heading">{{ item.heading }}</v-subheader>
+          </v-col>
+          <v-col cols="6" class="text-center">
+            <a href="#!" class="body-2 black--text">EDIT</a>
+          </v-col>
+        </v-row>
+        <v-list-group
+          v-else-if="item.children"
+          :key="item.text"
+          v-model="item.model"
+          :prepend-icon="item.model ? item.icon : item['icon-alt']"
+          append-icon
+        >
+          <template v-slot:activator>
+            <v-list-item-content>
+              <v-list-item-title>{{ item.text }}</v-list-item-title>
+            </v-list-item-content>
+          </template>
+          <v-list-item v-for="(child, i) in item.children" :key="i" link>
+            <v-list-item-action v-if="child.icon">
+              <v-icon>{{ child.icon }}</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>{{ child.text }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-group>
+        <v-list-item v-else :key="item.text" :to="item.route">
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>{{ item.text }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </template>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dialog: false,
+      items: [
+        { icon: "mdi-lightbulb", text: "Hue 제어", route: "/hue" },
+        { icon: "mdi-apple-icloud", text: "날씨 확인", route: "/weather" },
+        {
+          icon: "mdi-chevron-up",
+          "icon-alt": "mdi-chevron-down",
+          text: "More",
+          model: false,
+          children: [
+            { text: "Import" },
+            { text: "Export" },
+            { text: "Print" },
+            { text: "Undo changes" },
+            { text: "Other contacts" }
+          ]
+        }
+      ]
+    };
+  }
+};
+</script>
